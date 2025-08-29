181025856b31b32f23c8797553bbf9cd51a1c2a4
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-08-29 17:03:19 +0530

    chore(setup): add webpack project skeleton with loaders and plugins
    - Added base project structure with src and dist directories
    - Configured webpack with development server and source maps
    - Installed and setup css-loader, style-loader, html-webpack-plugin, and asset handling
    - Updated npm scripts for build and start

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|jpg|webp|svg|gif|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    open: true
+  }
+};
